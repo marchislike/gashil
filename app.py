ac1873e0bef2d35d1c87cffb9c14abc474950a45
Ha Jihyun <kimwatson2026@gmail.com>
2024-08-05 21:46:13 +0900

    #0 gashil mongoDB 설정

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://sparta:jungle@cluster0.sgh4pki.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-db = client.assignment
+client = MongoClient('mongodb+srv://kimwatson2026:HjvFSFwKAWPehGmd@gashil.yhejgv0.mongodb.net/?retryWrites=true&w=majority&appName=Gashil')
+db = client.gashil
 app = Flask(__name__)
 
 @app.route('/')
