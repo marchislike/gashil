5c70a75e3c5ca77f9a9dcd34fd7930be9d1e866d
Ha Jihyun <kimwatson2026@gmail.com>
2024-08-06 20:17:43 +0900

    #3 Nullable 항목구분, 삭제 상태의 글 안내문구 수정

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,7 @@ from bson.objectid import ObjectId
 import os
 
 from functools import wraps #*데코레이터로 DB connection check
-
-def convert_objectid_to_str(data):
-    if isinstance(data, list):
-        return [convert_objectid_to_str(item) for item in data]
-    elif isinstance(data, dict):
-        return {key: convert_objectid_to_str(value) for key, value in data.items()}
-    elif isinstance(data, ObjectId):
-        return str(data)
-    else:
-        return data
+from .utils import check_required_fields
 
 def create_app():
     app = Flask(__name__, template_folder=os.path.join(os.getcwd(), 'templates'))
@@ -44,7 +35,8 @@ def create_app():
     def get_posts():
         try:
             posts = list(app.db.posts.find())
-            posts = convert_objectid_to_str(posts)
+            for post in posts: #* Objectid를 사용하면서 MongoDB에서 json 직렬화가 가능하도록
+                post['_id'] = str(post['_id'])
             return jsonify(posts), 200
         except Exception as e:
             return jsonify({"error": str(e)}), 500
@@ -55,7 +47,7 @@ def create_app():
         try:
             post = app.db.posts.find_one({'_id': ObjectId(post_id)})
             if post:
-                post = convert_objectid_to_str(post)
+                post['_id'] = str(post['_id'])
                 return jsonify(post), 200
             else:
                 return jsonify({"error": "게시글을 찾을 수 없습니다."}), 404
@@ -68,6 +60,10 @@ def create_app():
     def create_post():
         try:
             data = request.get_json()
+            required_fields = ["departure", "arrival", "date", "limit"]
+            error_response = check_required_fields(required_fields, data)
+            if error_response:
+                return error_response
             user_id = data['user_id']
             post = {
                 "user_id": user_id,
@@ -80,6 +76,7 @@ def create_app():
                 "participants": [user_id]
             }
             result = app.db.posts.insert_one(post)
+            post['_id'] = str(result.inserted_id)
             return jsonify({"message": "게시글이 등록되었습니다.", "_id": str(result.inserted_id)}), 201
         except Exception as e:
             return jsonify({"error": str(e)}), 500
@@ -92,11 +89,17 @@ def create_app():
         try:
             data = request.get_json()
             update_fields = {key: value for key, value in data.items() if key != '_id'}
+            required_fields = ["departure", "arrival", "date", "limit"]
+            
+            error_response = check_required_fields(required_fields, update_fields)
+            if error_response:
+                return error_response
+            
             result = app.db.posts.update_one({'_id': ObjectId(post_id)}, {'$set': update_fields})
             if result.matched_count:
                 return jsonify({"message": "게시글이 수정되었습니다."}), 200
             else:
-                return jsonify({"error": "게시글을 찾을 수 없습니다."}), 404
+                return jsonify({"error": "이미 삭제된 게시글입니다."}), 404
         except Exception as e:
             return jsonify({"error": str(e)}), 500
 
@@ -108,7 +111,7 @@ def create_app():
             if result.deleted_count:
                 return jsonify({"message": "게시글이 삭제되었습니다."}), 200
             else:
-                return jsonify({"error": "게시글을 찾을 수 없습니다."}), 404
+                return jsonify({"error": "이미 삭제된 게시글입니다."}), 404
         except Exception as e:
             return jsonify({"error": str(e)}), 500
 
