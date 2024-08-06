d655009c44b277595575f6a70bbea0d7793b76b5
Soojin Lee <asyncwaiter@gmail.com>
2024-08-06 13:49:28 +0900

    refactor: InputForm 컴포넌트화

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+   return render_template('./pages/login.html')
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5001,debug=True)
