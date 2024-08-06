ac6a5480829faca80152f374bb388e662e527c67
Soojin Lee <asyncwaiter@gmail.com>
2024-08-06 16:34:59 +0900

    feat: Form Validation에 따른 UI 분기처리 추가

--- static/javascripts/member.js
+++ static/javascripts/member.js
@@ -1,16 +1,30 @@
-function checkMatchingPassword() {
-  let password = document.getElementById('password').value;
-  let confirmPassword = document.getElementById('password_confirm').value;
-  let warningText = document.getElementById('password_error');
+function validateForm() {
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('password_confirm').value;
+  const username = document.getElementById('username').value;
+  const nickname = document.getElementById('nickname').value;
+  const warningText = document.getElementById('password_error');
+  const requestBtn = document.getElementById('request_join_btn');
+
   if (password !== confirmPassword) {
     warningText.style.display = 'block';
+    requestBtn.setAttribute('disabled', true);
+    requestBtn.classList.remove('bg-Primary');
+    requestBtn.classList.add('bg-Low');
   } else {
     warningText.style.display = 'none';
+    requestBtn.removeAttribute('disabled');
+
+    if (username && nickname) {
+      requestBtn.classList.remove('bg-Low');
+      requestBtn.classList.add('bg-Primary');
+    } else {
+      requestBtn.classList.remove('bg-Primary');
+      requestBtn.classList.add('bg-Low');
+    }
   }
 }
 
-document.addEventListener('DOMContentLoaded', () =>
-  document
-    .getElementById('joinForm')
-    .addEventListener('input', checkMatchingPassword)
-);
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('joinForm').addEventListener('input', validateForm);
+});
