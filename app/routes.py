a71c7418109bc48878c8fd463893cafdecad7e67
Soojin Lee <asyncwaiter@gmail.com>
2024-08-07 17:55:28 +0900

    style: 글쓰기 UI 수정

--- app/routes.py
+++ app/routes.py
@@ -20,10 +20,10 @@ def login_page():
     return render_template('./pages/login.html')
 
 # POST
-@routes_bp.route('/new-post')
+@routes_bp.route('/posts/new')
 def new_post_page():
-    return render_template('') #new_post.html
+    return render_template('./pages/new.html', title='글쓰기')
 
-@routes_bp.route('/edit-post/<post_id>')
-def edit_post_page(post_id):
-    return render_template('', post_id=post_id) #edit_post.htm
+@routes_bp.route('/posts/<post_id>')
+def edit_post_page():
+    return render_template('')
